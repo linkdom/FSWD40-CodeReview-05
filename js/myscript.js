e2821a3451e74f84f3b53be0ce269fa215e44aa9
linko1994 <Vorbild94@gmail.com>
2018-05-12 01:03:08 +0200

    added Javascript like-function / still needs code improvement

---
+++ js/myscript.js
@@ -0,0 +1,36 @@
+var likesJohn = 0
+var likesJane = 0
+var likesMax = 0
+var likesMaxina = 0
+var likesIdk1 = 0
+var likesIdk2 = 0
+
+function likeJohn (){
+  likesJohn = likesJohn + 1
+  document.getElementById("john").innerHTML=likesJohn
+}
+
+function likeJane (){
+  likesJane = likesJane + 1
+  document.getElementById("jane").innerHTML=likesJane
+}
+
+function likeMax (){
+  likesMax = likesMax + 1
+  document.getElementById("max").innerHTML=likesMax
+}
+
+function likeMaxina (){
+  likesMaxina = likesMaxina + 1
+  document.getElementById("maxina").innerHTML=likesMaxina
+}
+
+function likeIdk1 (){
+  likesIdk1 = likesIdk1 + 1
+  document.getElementById("idk1").innerHTML=likesIdk1
+}
+
+function likeIdk2 (){
+  likesIdk2 = likesIdk2 + 1
+  document.getElementById("idk2").innerHTML=likesIdk2
+}
